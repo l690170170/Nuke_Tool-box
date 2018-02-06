2b6013ce0b6c235dcfb488bcbabef3a59c02525c
Joe_Lee <690170170@qq.com>
2018-02-06 18:58:42 +0800

    Bc_ToolSet

---
+++ Bc_ToolSet/init.py
@@ -0,0 +1,18 @@
+"""
+Script Name: init
+Version: 1.0
+Purpose: Adding Pr_Suite plugin paths.
+Created For: Bc_ToolSet v1.1
+Created On: 10/06/2016
+Author: Parimal Desai
+Website: http://www.bjqccm.com
+History:
+v1.0 (10/06/2016)
+    Adding Bc_ToolSet plugin paths.
+"""
+
+nuke.pluginAddPath("./python")
+nuke.pluginAddPath("./python/PrPy")
+nuke.pluginAddPath("./gizmos")
+nuke.pluginAddPath("./icons")
+nuke.pluginAddPath("./documentation")
